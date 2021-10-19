11e3624aa4b8d4ac446638308164f40c35a9d5d0
Eric Guttormson <eric.guttormson@live.com>
2021-10-18 21:14:45 -0500

    Initial Run

--- MyBlazorInAction03/BiA03FrmScrSvr/Server/Program.cs
+++ MyBlazorInAction03/BiA03FrmScrSvr/Server/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapFallbackToPage("/Host");
 
 app.Run();
