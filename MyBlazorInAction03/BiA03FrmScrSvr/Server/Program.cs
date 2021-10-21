ccfb2e91b4a51dcd0128eeb54cb940b6a54c14aa
Eric Guttormson <eric.guttormson@live.com>
2021-10-20 23:35:51 -0500

    add scratch projects

--- MyBlazorInAction03/BiA03FrmScrSvr/Server/Program.cs
+++ MyBlazorInAction03/BiA03FrmScrSvr/Server/Program.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
+app.MapBlazorHub();
 app.MapFallbackToPage("/Host");
 
 app.Run();
