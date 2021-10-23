5a0ad7cb52aa62a51b79e95c28f696fc20fbfc58
Eric Guttormson <eric.guttormson@live.com>
2021-10-23 00:04:25 -0500

    update project

--- MyBlazorInAction03/BiAFrmScrSvr01/Server/Program.cs
+++ MyBlazorInAction03/BiAFrmScrSvr01/Server/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/Host");
 
 app.Run();
