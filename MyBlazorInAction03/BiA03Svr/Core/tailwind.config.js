ba738b645b8d8f9487d3c575df7cc4857f00bab5
Eric Guttormson <eric.guttormson@live.com>
2021-10-24 23:29:47 -0500

    add navmeun

--- MyBlazorInAction03/BiA03Svr/Core/tailwind.config.js
+++ MyBlazorInAction03/BiA03Svr/Core/tailwind.config.js
@@ -9,25 +9,18 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    theme: {
-      screens: {
-        'xsm': '0px',
-        'sm': '320px',
-        'md': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'lg': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'xl': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        '2xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '3xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      }
+    screens: {
+      'xsm': '0px',
+      'sm': '320px',
+      'md': '640px', // => @media (min-width: 640px) { ... }
+
+      'lg': '768px', // => @media (min-width: 768px) { ... }
+
+      'xl': '1024px',  // => @media (min-width: 1024px) { ... }
+
+      '2xl': '1280px', // => @media (min-width: 1280px) { ... }
+
+      '3xl': '1536px', // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
