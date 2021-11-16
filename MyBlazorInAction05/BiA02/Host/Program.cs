87e6d49e061adba70406fa59c589365b9047421f
Eric Guttormson <eric.guttormson@live.com>
2021-11-15 22:22:43 -0600

    Add Nav Pages

--- MyBlazorInAction05/BiA02/Host/Program.cs
+++ MyBlazorInAction05/BiA02/Host/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapRazorPages();
+app.MapFallbackToPage("/Host");
 
 app.Run();
